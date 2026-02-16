c1f1aab02c4ab5f28ee9a3e968c2c87573b17deb
Tommy Crews <twcrews96@gmail.com>
2026-02-16 12:52:32 -0600

    Fix test.

--- Crews.Web.JsonApiClient.Tests/HttpResponseMessageExtensionsTests.cs
+++ Crews.Web.JsonApiClient.Tests/HttpResponseMessageExtensionsTests.cs
@@ -185,7 +185,7 @@ public class HttpResponseMessageExtensionsTests
 		Assert.NotNull(doc.JsonApi);
 		Assert.Equal("1.1", doc.JsonApi.Version);
 		Assert.NotNull(doc.Links);
-		Assert.NotNull(doc.Metadata);
+		Assert.NotNull(doc.Meta);
 		Assert.Equal("2024", doc.Meta["copyright"]!.GetValue<string>());
 	}
 }
